6734557e0c001c03805096c37cf3352353125eae
Dong  . Wang <n4c57f@tkdongu2.travp.net>
2018-06-13 13:22:42 -0400

    hello world

---
+++ excercises/01_hello.rb
@@ -0,0 +1,10 @@
+# Linux server with ruby installed ruby 01_hello.rb
+#
+# If Use Puppet Agent's Runtime:
+#       RHEL server:  /opt/puppetlabs/puppet/bin/ruby 01_hello.rb
+#       WIN server:  "C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin\ruby.exe" 01_hello.rb
+#
+# Linux Server with RubyRails   ruby 01_hello.rb
+# W10 client with rUBY Rails  C:/RailsInstaller/Ruby2.0.0/bin/ruby.exe 01_hello.rb
+#
+puts "Hello Wrold!"
